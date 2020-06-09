4ccc2d15f8effd9b65bb4aeb8ecda4dd6e2e34f1
justinliu <nhliupm@aliyun.com>
2020-06-09 18:48:03 +0800

    use JWT token store

---
+++ auth-server/src/main/java/com/example/authserver/config/AuthServerConfig.java
@@ -0,0 +1,72 @@
+package com.example.authserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+	@Autowired
+	private JwtAccessTokenConverter jwtAccessTokenConverter;
+
+	@Autowired
+	private JwtTokenStore jwtTokenStore;
+
+	@Override
+	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+	    clients
+				.inMemory()
+				.withClient("serviceClient")
+				.secret("{noop}secret")
+				.autoApprove(true)
+				.authorizedGrantTypes("client_credentials")
+				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+				.scopes("READ", "WRITE", "TRUST")
+				.and()
+				.withClient("resServer")
+				.secret("{noop}secret")
+				.autoApprove(true)
+				.authorizedGrantTypes("client_credentials")
+				.authorities("ROLE_CLIENT", "ROLE_TRUSTED_CLIENT")
+				.scopes("READ", "WRITE", "TRUST");
+	}
+ 
+	@Override
+	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+		endpoints
+				.tokenStore(jwtTokenStore)
+				.accessTokenConverter(jwtAccessTokenConverter);
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security
+				.tokenKeyAccess("permitAll()")
+				.checkTokenAccess("isAuthenticated()");
+	}
+
+	@Bean
+	public JwtTokenStore jwtTokenStore() {
+		return new JwtTokenStore(jwtAccessTokenConverter);
+	}
+
+	@Bean
+	@Primary
+	public JwtAccessTokenConverter jwtAccessTokenConverter(){
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setSigningKey("justinliu");
+		return converter;
+	}
+}
+
+
