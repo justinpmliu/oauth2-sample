dc248d594e1dff9f5b514d0626ca4a4177cc6a19
justinliu <nhliupm@aliyun.com>
2020-06-10 12:19:13 +0800

    use token_key endpoint to get public key

--- resource-server/src/main/java/com/example/res/server/config/SecurityConfig.java
+++ resource-server/src/main/java/com/example/res/server/config/SecurityConfig.java
@@ -1,24 +1,11 @@
 package com.example.res.server.config;
 
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
-import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.stream.Collectors;
 
 
 @Configuration
@@ -38,28 +25,6 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
 	  		.realmName("CRM_REALM");
     }
 
-	@Override
-	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
-		resources.tokenStore(new JwtTokenStore(accessTokenConverter()));
-	}
-
-	@Bean
-	public JwtAccessTokenConverter accessTokenConverter() {
-		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
-		Resource resource = new ClassPathResource("auth_public.txt");
-		String publicKey;
-		try {
-			publicKey = inputStream2String(resource.getInputStream());
-		} catch (final IOException e) {
-			throw new RuntimeException(e);
-		}
-		converter.setVerifierKey(publicKey);
-		return converter;
-	}
-
-	private String inputStream2String(InputStream in) {
-    	return new BufferedReader(new InputStreamReader(in, StandardCharsets.UTF_8)).lines().collect(Collectors.joining("\n"));
-	}
 }
 
 
