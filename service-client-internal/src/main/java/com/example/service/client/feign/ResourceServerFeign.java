3fa9a46a38331ef22f1499666b0b5e5c8fa40e01
justinliu <nhliupm@aliyun.com>
2021-03-26 15:17:07 +0800

    internal service client accesses auth server directly

--- service-client-internal/src/main/java/com/example/service/client/feign/ResourceServerFeign.java
+++ service-client-internal/src/main/java/com/example/service/client/feign/ResourceServerFeign.java
@@ -1,12 +1,11 @@
 package com.example.service.client.feign;
 
-import com.example.service.client.config.CustomFeignSecurityConfig;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(value = "RESOURCE-SERVER", configuration = CustomFeignSecurityConfig.class)
+@FeignClient(value = "RESOURCE-SERVER")
 public interface ResourceServerFeign {
 
     @RequestMapping(value="/hello", method = RequestMethod.POST)
--- service-client/src/main/java/com/example/service/client/controller/ServiceController.java
+++ service-client/src/main/java/com/example/service/client/controller/ServiceController.java
@@ -2,21 +2,18 @@ package com.example.service.client.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestOperations;
-import org.springframework.http.ResponseEntity;
 
 
 @RestController
 public class ServiceController {
 
     @Autowired
-    @Qualifier("myRestTemplate")
     private RestOperations template;
 	
     @RequestMapping(value="/test",method=RequestMethod.GET)
