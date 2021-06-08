3320dc4016ed12e89a62683e9121b4de69ebf4ab
justinliu <nhliupm@aliyun.com>
2021-06-08 17:38:03 +0800

    make resource-server be standalone

--- service-client/src/main/java/com/example/service/client/controller/ServiceController.java
+++ service-client/src/main/java/com/example/service/client/controller/ServiceController.java
@@ -18,7 +18,7 @@ public class ServiceController {
 	
     @RequestMapping(value="/test",method=RequestMethod.GET)
     public String getShipmentDetails(@RequestParam(value="name", defaultValue="0") String name) {
-        ResponseEntity<String> result = template.postForEntity("http://localhost:8080/resource-server/hello",name,String.class);
+        ResponseEntity<String> result = template.postForEntity("http://localhost:8080/api/hello",name,String.class);
         return result.getBody();
     }
     
